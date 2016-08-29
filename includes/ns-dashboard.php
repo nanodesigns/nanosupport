44a76bd56559636cb083606d8a29cf5febd18bd3
Mayeenul Islam <wz.islam@gmail.com>
2016-08-30 05:41:25 +0600

    In-plugin Support URL is directed to github issues

--- includes/ns-dashboard.php
+++ includes/ns-dashboard.php
@@ -246,7 +246,7 @@ function nanosupport_widget_callback() { ?>
 
         <hr>
         <div class="ns-text-center">
-            <?php printf( __( '<a href="%1s" targe="_blank">Rate us %2s</a>', 'nanosupport' ), 'https://wordpress.org/support/view/plugin-reviews/nanosupport#postform' , '<i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i>' ); ?> | <?php printf( __( '<a href="%1s" targe="_blank">Get Support</a>', 'nanosupport' ), 'https://wordpress.org/support/plugin/nanosupport' ); ?> | <a href="http://nanodesignsbd.com?ref=nanosupport" target="_blank" title="nanodesigns - developer of the plugin"><strong>nano</strong>designs</a>
+            <?php printf( __( '<a href="%1s" targe="_blank">Rate us %2s</a>', 'nanosupport' ), 'https://wordpress.org/support/view/plugin-reviews/nanosupport#postform' , '<i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i><i class="ns-icon-star-filled"></i>' ); ?> | <?php printf( __( '<a href="%1s" targe="_blank">Get Support</a>', 'nanosupport' ), 'https://github.com/nanodesigns/nanosupport/issues/new' ); ?> | <a href="http://nanodesignsbd.com?ref=nanosupport" target="_blank" title="nanodesigns - developer of the plugin"><strong>nano</strong>designs</a>
         </div>
 
     </section>
